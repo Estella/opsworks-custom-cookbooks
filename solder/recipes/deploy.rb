ed2ad2d4edabfdcf9cf46a33e8a1d3f0f3e47cc0
sct <sctsnipe@gmail.com>
2013-02-24 22:11:08 -0500

    Solder Instance Deployment

--- solder/recipes/deploy.rb
+++ solder/recipes/deploy.rb
@@ -1,17 +1,27 @@
 node[:deploy].each do |app_name, deploy|
 
-  script "install_composer" do
+  script "copy_config" do
     interpreter "bash"
     user "root"
-    cwd "#{deploy[:deploy_to]}/current"
+    cwd "#{deploy[:deploy_to]}/current/application"
     code <<-EOH
-    curl -s https://getcomposer.org/installer | php
-    php composer.phar install
+    cp -r config-sample/ config/
+    rm -f config/database.php
+    rm -f config/solder.php
     EOH
   end
 
-  template "#{deploy[:deploy_to]}/current/db-connect.php" do
-    source "db-connect.php.erb"
+  script "copy_htaccess" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current/public"
+    code <<-EOH
+    cp .htaccess-sample .htaccess
+    EOH
+  end
+
+  template "#{deploy[:deploy_to]}/current/application/config/database.php" do
+    source "database.php.erb"
     mode 0660
     group deploy[:group]
 
@@ -25,8 +35,27 @@ node[:deploy].each do |app_name, deploy|
       :host =>     (deploy[:database][:host] rescue nil),
       :user =>     (deploy[:database][:username] rescue nil),
       :password => (deploy[:database][:password] rescue nil),
-      :db =>       (deploy[:database][:database] rescue nil),
-      :table =>    (node[:phpapp][:dbtable] rescue nil)
+      :db =>       (deploy[:database][:database] rescue nil)
+    )
+
+   only_if do
+     File.directory?("#{deploy[:deploy_to]}/current")
+   end
+  end
+
+  template "#{deploy[:deploy_to]}/current/application/config/solder.php" do
+    source "solder.php.erb"
+    mode 0660
+    group deploy[:group]
+
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")   
+      owner "apache"
+    end
+
+    variables(
+      :repo_url => (node[:repo_url] rescue nil)
     )
 
    only_if do
