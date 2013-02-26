80ec707fb354fe813ef8e495b6f6107a3fbafa7a
sct <sctsnipe@gmail.com>
2013-02-26 13:53:47 -0500

    Recipes for Platform

---
+++ platform/recipes/deploy.rb
@@ -0,0 +1,43 @@
+node[:deploy].each do |app_name, deploy|
+
+  script "set_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    chmod -R 777 storage
+    EOH
+  end
+
+  template "#{deploy[:deploy_to]}/current/application/config/database.php" do
+    source "database.php.erb"
+    mode 0660
+    group deploy[:group]
+
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")   
+      owner "apache"
+    end
+
+    variables(
+      :host =>     (deploy[:database][:host] rescue nil),
+      :user =>     (deploy[:database][:username] rescue nil),
+      :password => (deploy[:database][:password] rescue nil),
+      :db =>       (deploy[:database][:database] rescue nil)
+    )
+
+   only_if do
+     File.directory?("#{deploy[:deploy_to]}/current")
+   end
+  end
+
+  script "run_migrations" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    php artisan migrate
+    EOH
+  end
+end
