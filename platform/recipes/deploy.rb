4e502e93f590aca10c2ce35b86be007cf6fd78d8
sct <sctsnipe@gmail.com>
2013-02-27 03:52:57 -0500

    Adjustments for memcached

--- platform/recipes/deploy.rb
+++ platform/recipes/deploy.rb
@@ -32,6 +32,22 @@ node[:deploy].each do |app_name, deploy|
      end
     end
 
+    template "#{deploy[:deploy_to]}/current/application/config/cache.php" do
+      source "cache.php.erb"
+      mode 0660
+      group deploy[:group]
+
+      if platform?("ubuntu")
+        owner "www-data"
+      elsif platform?("amazon")   
+        owner "apache"
+      end
+
+     only_if do
+       File.directory?("#{deploy[:deploy_to]}/current")
+     end
+    end
+
     script "run_migrations" do
       interpreter "bash"
       user "root"
