b71814b34363d43a7388b3e6417ed5b9a1bce184
sct <sctsnipe@gmail.com>
2013-02-27 03:02:41 -0500

    Forum recipe

---
+++ forums/recipes/deploy.rb
@@ -0,0 +1,35 @@
+node[:deploy].each do |app_name, deploy|
+  if deploy[:application] == "technic_forums"
+    script "pre_configure" do
+      interpreter "bash"
+      user "root"
+      cwd "#{deploy[:deploy_to]}/current"
+      code <<-EOH
+      ln -s /vol/repo/forumdata/data data
+      EOH
+    end
+
+    template "#{deploy[:deploy_to]}/current/library/config.php" do
+      source "config.php.erb"
+      mode 0660
+      group deploy[:group]
+
+      if platform?("ubuntu")
+        owner "www-data"
+      elsif platform?("amazon")   
+        owner "apache"
+      end
+
+      variables(
+        :host =>     (deploy[:database][:host] rescue nil),
+        :user =>     (deploy[:database][:username] rescue nil),
+        :password => (deploy[:database][:password] rescue nil),
+        :db =>       (deploy[:database][:database] rescue nil)
+      )
+
+     only_if do
+       File.directory?("#{deploy[:deploy_to]}/current")
+     end
+    end
+  end
+end
